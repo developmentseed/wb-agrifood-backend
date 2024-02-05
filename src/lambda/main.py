a186edd5b354e2a4c2087ca195a85be872697b4d
Wille Marcel <wille.yyz@gmail.com>
2024-02-05 12:24:09 -0300

    Add /thread and /thread/{id} endpoints

--- src/lambda/main.py
+++ src/lambda/main.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 import os
+from datetime import datetime
 
 import lancedb
 from fastapi import FastAPI
 from mangum import Mangum
-from models import Answer
-from models import Prompt
+from models import Answer, Thread, Prompt
 from openai import OpenAI
 
 # from dotenv import load_dotenv
@@ -59,6 +59,33 @@ def openai_request(prompt: Prompt):
         ],
     )
     return Answer(text=response.choices[0].message.content)
+
+
+@app.post("/thread/")
+def create_thread():
+    thread = client.beta.threads.create()
+    return Thread(id=thread.id, created=datetime.fromtimestamp(thread.created_at))
+
+
+@app.post("/thread/{id}")
+def add_to_thread(id: str, prompt: Prompt):
+    client.beta.threads.messages.create(
+        thread_id=id,
+        role="user",
+        content=prompt.text,
+    )
+    assistant = [
+        assistant
+        for assistant in client.beta.assistants.list()
+        if assistant.name == OPENAI_ASSISTANT_NAME
+    ]
+    if not prompt.run_id:
+        run = client.beta.threads.runs.create(thread_id=id, assistant_id=assistant[0].id)
+
+    run = client.beta.threads.runs.retrieve(thread_id=id, run_id=prompt.run_id or run.id)
+    reply = client.beta.threads.messages.list(thread_id=id)
+
+    return reply.data
 
 
 handler = Mangum(app, lifespan='off')
--- src/lambda/models.py
+++ src/lambda/models.py
@@ -1,8 +1,16 @@
+from typing import Optional
+from datetime import datetime
 from pydantic import BaseModel
 
 
 class Prompt(BaseModel):
     text: str
+    run_id: Optional[str] = None
+
+
+class Thread(BaseModel):
+    id: str
+    created: datetime
 
 
 class Answer(BaseModel):
